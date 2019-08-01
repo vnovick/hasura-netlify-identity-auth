c7ea7ca628337715f37de0f21e72f38492541418
vnovick <vnovick@gmail.com>
2019-08-01 13:12:45 +0300

    cleanup

--- functions/validate-user/validate-user.js
+++ functions/validate-user/validate-user.js
@@ -22,29 +22,4 @@ exports.handler = async (event, context) => {
       "X-Hasura-role": "anonymous"
     })
   };
-  //return anonymous role
-
-  // try {
-  //   return fetch(userUrl, {
-  //     method: "GET",
-  //     headers: { Authorization: adminAuthHeader }
-  //   })
-  //     .then(response => {
-  //       return response.json();
-  //     })
-  //     .then(data => {
-  //       console.log("data", JSON.stringify(data));
-  //       return { statusCode: 204 };
-  //     })
-  //     .catch(e => {
-  //       console.log("Failed to get user! 500! Internal.");
-  //       return {
-  //         statusCode: 500,
-  //         body: "Internal Server Error: " + e
-  //       };
-  //     });
-  // } catch (e) {
-  //   console.log("GOT HERE! 500! outer");
-  //   return { statusCode: 500, body: "Internal Server Error: " + e };
-  // }
 };
